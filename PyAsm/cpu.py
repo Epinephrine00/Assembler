e5bd2d97de32779576a030d1f49110877967059b
dpvpd <61593643+dpvpd@users.noreply.github.com>
2024-03-13 04:17:27 +0900

    0312_beforesleepCommit

--- PyAsm/cpu.py
+++ PyAsm/cpu.py
@@ -1,32 +1,96 @@
 from memory import Memory
 
-
 class CPU:
 
-    __InstructionTable = {}
+    # 범용 레지스터 초기화
+    Accumulator = 0x0000
+    AX = lambda: CPU.Accumulator
+    AH = lambda: CPU.Accumulator>>8
+    AL = lambda: CPU.Accumulator&0xFF
 
-    # Universal Registor
-    __Accumulator = 0x0000
-    AX = lambda: CPU.__Accumulator
-    AH = lambda: CPU.__Accumulator>>8
-    AL = lambda: CPU.__Accumulator&0xFF
+    BaseReg = 0x0000
+    BX = lambda: CPU.BaseReg
+    BH = lambda: CPU.BaseReg>>8
+    BL = lambda: CPU.BaseReg&0xFF
 
-    __BaseReg = 0x0000
-    BX = lambda: CPU.__BaseReg
+    CountReg = 0x0000
+    CX = lambda: CPU.CountReg
+    CH = lambda: CPU.CountReg>>8
+    CL = lambda: CPU.CountReg&0xFF
 
-    __CountReg = 0x0000
-    CX = lambda: CPU.__CountReg
+    DataReg = 0x0000
+    DX = lambda: CPU.DataReg
+    DH = lambda: CPU.DataReg>>8
+    DL = lambda: CPU.DataReg&0xFF
 
-    __DataReg = 0x0000
-    DX = lambda: CPU.__DataReg
 
-    __InstructionPointer = 0x0000
-    IP = lambda: CPU.__InstructionPointer
+    # 강의 내용상, 스택 영역이 없는 16비트 CPU를 상정하고 있으므로
+    # 스택 포인터 (SP) 및 베이스 포인터(BP) 레지스터는 갖지 않는다. 
+    # 또한 SI/DI 레지스터는 현 단계에서 필요치 않다고 판단하여 추가하지 않았다. 
+    InstructionPointer = 0x0000
+    IP = lambda: CPU.InstructionPointer
 
-    def SetInstructionPointer(address):
+    # IndexPointer = 0x0000
+    # IX = lambda: CPU.IndexPointer
+
+    def SetInstructionPointer(self, address):
         CodeSegmentOffset = Memory.CodeSegmentOffset
-        CPU.__InstructionPointer = address + CodeSegmentOffset
+        CPU.InstructionPointer = address + CodeSegmentOffset
 
-    def ALU():
-        Instruction = Memory.getDataAt(CPU.IP())
+    def ALU(self):
+        Instruction = Memory.getWordByAddress(CPU.IP())
+        InstructionType = Instruction>>12
         
+        if InstructionType&0x7000:
+            self.RegRefInst(Instruction)
+        elif Instruction&0xF000:
+            self.IOInst(Instruction)
+        elif Instruction&0x8000:
+            self.MemRefInst_IndAddress(Instruction)
+        else:
+            self.MemRefInst_DirAddress(Instruction)
+
+    def RegRefInst(self, Instruction):
+        pass
+    def IOInst(self, Instruction):
+        pass
+    def MemRefInst_IndAddress(self, Instruction):
+        pass
+    def MemRefInst_DirAddress(self, Instruction):
+        pass
+
+
+
+    def AND(self, address):
+        CPU.DataReg = Memory.getWordByAddress(address)
+        CPU.Accumulator = CPU.AX() & CPU.DX()
+
+    def FullAdder(self, a, b, cin):
+        a = a&1
+        b = b&1
+        cin = cin&1
+        s = (a^b)^cin
+        cout = (a&b) | ((a^b)&cin)
+        return (s, cout) #절대로 사칙연산 안쓰기
+    def ADD(self, address):
+        result = 0
+        CPU.DataReg = Memory.getWordByAddress(address)
+        for i in range(16): # 반복문은 반칙같아서 쓰기 싫었지만, 숫자만 다른 같은 코드의 반복이라 "귀찮아서" 반복문 사용
+            s, c = self.FullAdder(CPU.AX()>>i&1, CPU.DX()>>i&1, c)
+            result |= s<<i
+        # 최상위 비트(16번 비트)는 16비트 시스템에선 무시하므로 최종 캐리를 더해주지 않음
+        CPU.Accumulator = result&0xffff #그래도 혹시모르니 16비트로 고정
+    def LDA(self, address):
+        CPU.Accumulator = Memory.getWordByAddress(address)
+    
+    def STA(self, address):
+        Memory.setMemory(address, CPU.AX())
+
+    def BUN(self, address):
+        CodeSegmentOffset = Memory.CodeSegmentOffset
+        CPU.InstructionPointer = address + CodeSegmentOffset
+    
+    def BSA(self, address):
+        returnAddress = CPU.IP()
+        CPU.InstructionPointer = address
+        Memory.pushStack(returnAddress)
--- PyAsm/memory.py
+++ PyAsm/memory.py
@@ -1,6 +1,6 @@
 
 class Memory:
-    memory = []*(2**16) # 16-bit Address bus : 0~(2^16)-1 range of memory
+    memory = [0]*(2**16) # 16-bit Address bus : 0~(2^16)-1 range of memory
 
     # Memory mode selector const : Use when Set or get Memory
     BIN = 0
@@ -10,10 +10,18 @@ class Memory:
 
     # Offsets
     CodeSegmentOffset = 0x0000
+    DataSegmentOffset = 0x0000
+
 
     def __init__(self, address, isPointer=False):
         self.address = address
         self.isPointer = isPointer
     
-    def setMemory(mode:int=2):
-        Memory.BIN
+    def setMemory(self, address, value):
+        pass
+
+    def pushStack(self, value):
+        pass
+
+    def getWordByAddress(self, address):
+        result = ((Memory.memory[address]&1)<<7) | ((Memory.memory[address+1]&1)<<6) | ((Memory.memory[address+2]&1)<<5) | ((Memory.memory[address+3]&1)<<4) | ((Memory.memory[address+4]&1)<<3) | ((Memory.memory[address+5]&1)<<2) | ((Memory.memory[address+6]&1)<<1)| (Memory.memory[address+7]&1)
