f9c8c43b58c5a2b9b5e112ddcb0819509c4ca916
dpvpd <61593643+dpvpd@users.noreply.github.com>
2024-03-17 21:35:16 +0900

    asm testcode added

---
+++ helloworld.asm
@@ -0,0 +1,40 @@
+    ORG 0
+    CLA
+    LDA H
+    OUT
+    LDA E
+    OUT
+    LDA L
+    OUT
+    LDA L
+    OUT
+    LDA O
+    OUT
+    LDA CM
+    OUT
+    LDA SP
+    OUT
+    LDA W
+    OUT
+    LDA O
+    OUT
+    LDA R
+    OUT
+    LDA L
+    OUT
+    LDA D
+    OUT
+    LDA EM
+    OUT
+    HLT    
+H,  HEX 48
+E,  HEX 45
+L,  HEX 4C
+O,  HEX 4F
+CM, HEX 2C
+SP, HEX 20
+W,  HEX 57
+R,  HEX 53
+D,  HEX 44
+EM, HEX 21
+    END
