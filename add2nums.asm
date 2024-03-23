a35098ce6e7339e6e163b98379b165185eea78f4
dpvpd <61593643+dpvpd@users.noreply.github.com>
2024-03-23 23:51:23 +0900

    설명 추가

--- add2nums.asm
+++ add2nums.asm
@@ -1,9 +1,9 @@
-        ORG 0
-        INP
-        STA A
-        INP
-        ADD A
-        OUT
-        HLT
-A,      HEX 0
-        END
+    ORG 0
+    INP
+    STA A
+    INP
+    ADD A
+    OUT
+    HLT
+A,  HEX 0
+    END
